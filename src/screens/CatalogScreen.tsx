d6b6c964afa0769be263735a10b2a2c29098d612
patrykbaranek <cien2460045@gmail.com>
2023-03-22 22:30:44 +0100

    add safeareaview

--- src/screens/CatalogScreen.tsx
+++ src/screens/CatalogScreen.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { CatalogGamesRenderer } from '../components/gamelist/CatalogGamesRenderer';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const CatalogScreen = () => {
-  return <CatalogGamesRenderer />;
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+      <CatalogGamesRenderer />
+    </SafeAreaView>
+  );
 };
