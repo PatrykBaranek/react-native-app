6a9ab6bdd074797695c15a79a89e870061133c0a
patrykbaranek <cien2460045@gmail.com>
2023-03-22 22:30:09 +0100

    remove title view

--- src/screens/GamelistScreen.tsx
+++ src/screens/GamelistScreen.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Gamelist } from '../components/gamelist/Gamelist';
 
 export const GamelistScreen = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
-      <View style={{ padding: 20, backgroundColor: '#000' }}>
-        <Text style={{ color: '#fff', textAlign: 'center', fontSize: 24 }}>Gamelist</Text>
-      </View>
       <Gamelist />
     </SafeAreaView>
   );
