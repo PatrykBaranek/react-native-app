a12e65303c7512494110dff05d64326ae7749d14
patrykbaranek <cien2460045@gmail.com>
2023-03-22 22:31:19 +0100

    add badge component to see length of games in catalog

--- src/components/gamelist/CatalogsRenderer.tsx
+++ src/components/gamelist/CatalogsRenderer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FlatList, ListRenderItemInfo, TouchableHighlight, View } from 'react-native';
+import { FlatList, ListRenderItemInfo, TouchableHighlight, View, StyleSheet } from 'react-native';
 import { CatalogType } from '../../app/gamelistSlice/gamelistSlice';
-import { IconButton, Text } from 'react-native-paper';
+import { Badge, IconButton, Text } from 'react-native-paper';
 import { useAppSelector } from '../../app/hooks';
 import { useNavigation } from '@react-navigation/native';
 import { GamelistScreenNavigationProps } from '../../navigation/GamelistStackScreen';
@@ -12,20 +12,32 @@ export const CatalogsRenderer = () => {
 
   const renderItem = ({ item: catalog }: ListRenderItemInfo<CatalogType>) => {
     return (
-      <TouchableHighlight onPress={() => navigation.navigate('Catalog', { id: catalog.id })}>
-        <View>
+      <TouchableHighlight
+        onPress={() => navigation.navigate('Catalog', { id: catalog.id, name: catalog.name })}
+      >
+        <View style={styles.catalogItem}>
+          {catalog.games.length > 0 && (
+            <Badge style={{ position: 'absolute', top: 0, right: 20, zIndex: 9999 }}>
+              {catalog.games.length}
+            </Badge>
+          )}
           <IconButton icon="folder" mode="contained" />
-          <Text style={{ color: '#fff' }}>{catalog.name}</Text>
+          <Text style={styles.catalogText}>{catalog.name}</Text>
         </View>
       </TouchableHighlight>
     );
   };
 
   return (
-    <FlatList
-      data={catalogs}
-      renderItem={renderItem}
-      style={{ flexDirection: 'row', gap: 10, padding: 20 }}
-    />
+    <FlatList data={catalogs} renderItem={renderItem} contentContainerStyle={styles.conatiner} />
   );
 };
+
+const styles = StyleSheet.create({
+  conatiner: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  catalogItem: { position: 'relative', height: 100, width: 100, alignItems: 'center' },
+  catalogText: { color: '#fff', fontSize: 16 },
+});
